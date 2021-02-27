3fc06aed3c9efb95845c958fb39d62fed9bb4ee5
Oleg <oleg.parkhom@gmail.com>
2021-02-27 14:39:23 +0200

    Подключает redux к проекту

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
 import App from "./components/App";
+import {reducer} from './store/reducer';
 
 const Setting = {
   AD_COUNT: 312,
   CITIES: [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`]
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      adCount={Setting.AD_COUNT}
-      cities={Setting.CITIES}
-      offers={offers.slice()}
-      reviews={reviews.slice()}
-    />,
+    <Provider store={store}>
+      <App
+        adCount={Setting.AD_COUNT}
+        cities={Setting.CITIES}
+        offers={offers.slice()}
+        reviews={reviews.slice()}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
