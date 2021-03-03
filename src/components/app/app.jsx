9fae14fe9e5068908e00e174839f404f67b16949
Oleg <oleg.parkhom@gmail.com>
2021-03-03 22:03:22 +0200

    Переименовывает файл компонента избранных страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import MainPage from "../pages/main-page/main-page";
-import FavoritesPage from "../pages/favorites-page";
+import FavoritesPage from "../pages/favorites-page/favorites-page";
 import LoginPage from "../pages/login-page/login-page";
 import OfferPage from "../pages/offer-page/offer-page";
 import NotFoundPage from "../pages/not-found-page/not-found-page";
